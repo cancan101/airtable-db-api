c4655e964cc9362fa5bb7c154d50d52384129c98
Alex Rothberg <agrothberg@gmail.com>
2022-03-22 16:47:35 -0400

    more testing

--- tests/test_dialect.py
+++ tests/test_dialect.py
@@ -1,5 +1,14 @@
-from sqlalchemy.engine import Engine
+from sqlalchemy import inspect
+from sqlalchemy.engine import Connection, Engine
 
 
 def test_create_engine(engine: Engine) -> None:
     pass
+
+
+def test_get_table_names(connection_with_meta: Connection) -> None:
+    insp = inspect(connection_with_meta)
+
+    tables = insp.get_table_names()
+
+    assert "name1" in tables
