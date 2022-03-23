2e4319029ef08ab3917ee297f1d3941be3b36bd8
Alex Rothberg <agrothberg@gmail.com>
2022-03-23 18:07:27 -0400

    Handle Airtable types (#4)

---
+++ airtabledb/fields.py
@@ -0,0 +1,41 @@
+from typing import List, Optional, Union
+
+from shillelagh.fields import Field
+
+# -----------------------------------------------------------------------------
+
+AirtableRawInputTypes = Union[str, int, float]
+AirtableInputTypes = Union[AirtableRawInputTypes, List[AirtableRawInputTypes]]
+AirtablePrimitiveTypes = Union[str, int, float]
+
+
+class MaybeListString(
+    Field[AirtableInputTypes, AirtablePrimitiveTypes]  # type: ignore
+):
+    # These types are not really "correct" given the polymorphism
+    type = "TEXT"
+    db_api_type = "STRING"
+
+    def parse(
+        self, value: Optional[AirtableInputTypes]
+    ) -> Optional[AirtablePrimitiveTypes]:
+        if value is None:
+            return None
+
+        if isinstance(value, (str, int, float)):
+            return value
+        elif isinstance(value, list):
+            if len(value) == 0:
+                return None
+            elif len(value) == 1:
+                ret = value[0]
+                # TODO(cancan101): Do we have to handle nested arrays / special types?
+                if ret is not None and not isinstance(ret, (str, int, float)):
+                    raise TypeError(f"Unknown type: {type(ret)}")
+                return ret
+            else:
+                raise ValueError("Unable to handle list of length > 1")
+        elif value == {"specialValue": "NaN"}:
+            return None
+        else:
+            raise TypeError(f"Unknown type: {type(value)}")
---
+++ tests/test_fields.py
@@ -0,0 +1,35 @@
+import pytest
+
+from airtabledb.fields import MaybeListString
+
+
+def test_maybe_list_string_none():
+    assert MaybeListString().parse(None) is None
+
+
+def test_maybe_list_string_primitive():
+    assert MaybeListString().parse("a") == "a"
+    assert MaybeListString().parse(1) == 1
+    assert MaybeListString().parse(1.5) == 1.5
+
+
+def test_maybe_list_string_list():
+    assert MaybeListString().parse([]) is None
+    assert MaybeListString().parse(["a"]) == "a"
+    assert MaybeListString().parse([1]) == 1
+    assert MaybeListString().parse([1.5]) == 1.5
+    assert MaybeListString().parse([None]) is None
+
+    with pytest.raises(TypeError):
+        assert MaybeListString().parse([{}])
+
+    # At some point we may handle this case differently (w/o error)
+    with pytest.raises(ValueError):
+        assert MaybeListString().parse([1, 2])
+
+
+def test_maybe_list_string_special():
+    assert MaybeListString().parse({"specialValue": "NaN"}) is None
+
+    with pytest.raises(TypeError):
+        MaybeListString().parse({"specialValue": "XXX"})
