57c496d89b1c88cd7e599ce5bab94529f7514cc4
Alex Rothberg <agrothberg@gmail.com>
2022-03-28 19:05:38 -0400

    break up tests

--- tests/test_formula.py
+++ tests/test_formula.py
@@ -3,16 +3,21 @@ from shillelagh import filters
 from airtabledb.formulas import get_airtable_formula, get_formula
 
 
-def test_get_formula():
+def test_get_formula_is_null():
     assert (
         get_formula("the field", filters.IsNull())
         == 'IF({the field} & "", FALSE(), TRUE())'
     )
+
+
+def test_get_formula_is_not_null():
     assert (
         get_formula("the field", filters.IsNotNull())
         == 'IF({the field} & "", TRUE(), FALSE())'
     )
 
+
+def test_get_formula_range():
     assert get_formula("the field", filters.Range(start=0)) == "AND({the field} > 0)"
     assert (
         get_formula("the field", filters.Range(start=0, include_start=True))
@@ -35,4 +40,11 @@ def test_get_airtable_formula():
     assert (
         get_airtable_formula({"the field": filters.IsNull()})
         == 'AND(IF({the field} & "", FALSE(), TRUE()))'
+    )
+
+    assert (
+        get_airtable_formula(
+            {"the field": filters.IsNull(), "other field": filters.Range(start=2)}
+        )
+        == 'AND(IF({the field} & "", FALSE(), TRUE()),AND({other field} > 2))'
     )
