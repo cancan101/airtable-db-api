811c19f3959bfea5431ec022f06cd10d3a71709b
Alex Rothberg <agrothberg@gmail.com>
2022-03-28 13:51:27 -0400

    Move sorting to Airtable (#10)

--- airtabledb/adapter.py
+++ airtabledb/adapter.py
@@ -3,13 +3,20 @@ from typing import Any, Dict, Iterable, Iterator, List, Optional, Tuple
 
 from pyairtable import Table
 from shillelagh.adapters.base import Adapter
-from shillelagh.fields import Boolean, Field, Filter, Float, String
+from shillelagh.fields import Boolean, Field, Filter, Float, Order, String
 from shillelagh.typing import RequestedOrder
 
 from .fields import MaybeListString
-from .types import BaseMetadata
+from .types import BaseMetadata, TypedDict
 
 # -----------------------------------------------------------------------------
+
+
+class FieldKwargs(TypedDict, total=False):
+    order: Order
+
+
+FIELD_KWARGS: FieldKwargs = {"order": Order.ANY}
 
 
 def guess_field(values: List[Any]) -> Field:
@@ -17,25 +24,29 @@ def guess_field(values: List[Any]) -> Field:
     if len(types) == 1:
         types0 = list(types)[0]
         if types0 is str:
-            return String()
+            return String(**FIELD_KWARGS)
         elif types0 is float:
-            return Float()
+            return Float(**FIELD_KWARGS)
         elif types0 is int:
             # This seems safest as there are cases where we get floats and ints
-            return Float()
+            return Float(**FIELD_KWARGS)
         elif types0 is bool:
-            return Boolean()
+            return Boolean(**FIELD_KWARGS)
         elif types0 is list:
             # TODO(cancan101): do more work + make a Field for this
-            return MaybeListString()
+            return MaybeListString(**FIELD_KWARGS)
     elif types == {float, int}:
-        return Float()
+        return Float(**FIELD_KWARGS)
     elif types == {float, dict} or types == {int, dict} or types == {int, float, dict}:
         # TODO(cancan101) check the dict + make a Field for this
         # This seems safest as there are cases where we get floats and ints
-        return MaybeListString()
+        return MaybeListString(**FIELD_KWARGS)
 
-    return MaybeListString()
+    return MaybeListString(**FIELD_KWARGS)
+
+
+def get_airtable_sort(order: List[Tuple[str, RequestedOrder]]) -> List[str]:
+    return [(s if o is Order.ASCENDING else f"-{s}") for s, o in order]
 
 
 class AirtableAdapter(Adapter):
@@ -71,7 +82,7 @@ class AirtableAdapter(Adapter):
             fields = [col["name"] for col in columns_metadata]
             self.strict_col = True
 
-            columns = dict({k: MaybeListString() for k in fields}, id=String())
+            columns = {k: MaybeListString(**FIELD_KWARGS) for k in fields}
 
         # Attempts introspection by looking at data.
         # This is super not reliable
@@ -98,11 +109,9 @@ class AirtableAdapter(Adapter):
 
             self.strict_col = False
 
-            columns = dict(
-                {k: guess_field(v) for k, v in field_values.items()}, id=String()
-            )
+            columns = {k: guess_field(v) for k, v in field_values.items()}
 
-        self.columns = columns
+        self.columns = dict(columns, id=String())
 
     @staticmethod
     def supports(uri: str, fast: bool = True, **kwargs: Any) -> Optional[bool]:
@@ -121,7 +130,8 @@ class AirtableAdapter(Adapter):
         bounds: Dict[str, Filter],
         order: List[Tuple[str, RequestedOrder]],
     ) -> Iterator[Dict[str, Any]]:
-        for page in self._table_api.iterate():
+        sort = get_airtable_sort(order)
+        for page in self._table_api.iterate(sort=sort):
             for result in page:
                 yield dict(
                     {
