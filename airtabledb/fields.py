8f5a8b733439e4a4791b4b648b5e42a164b09b8a
Alex Rothberg <agrothberg@gmail.com>
2022-03-30 15:54:05 -0400

    cleanup fields (#23)

--- airtabledb/fields.py
+++ airtabledb/fields.py
@@ -1,12 +1,28 @@
+import math
 from typing import List, Optional, Union
 
-from shillelagh.fields import Field
+from shillelagh.fields import Field, String
+
+from .types import TypedDict
 
 # -----------------------------------------------------------------------------
 
-AirtableRawInputTypes = Union[str, int, float]
+
+class AirtableFloatType(TypedDict):
+    specialValue: str
+
+
+AirtableRawNumericInputTypes = Union[int, float, AirtableFloatType]
+AirtableRawInputTypes = Union[str, AirtableRawNumericInputTypes]
 AirtableInputTypes = Union[AirtableRawInputTypes, List[AirtableRawInputTypes]]
 AirtablePrimitiveTypes = Union[str, int, float]
+
+
+SPECIAL_VALUE_KEY = "specialValue"
+
+NAN_REPRESENTATION = AirtableFloatType(specialValue="NaN")
+INF_REPRESENTATION = AirtableFloatType(specialValue="Infinity")
+INF_NEG_REPRESENTATION = AirtableFloatType(specialValue="-Infinity")
 
 
 class MaybeListString(
@@ -16,26 +32,96 @@ class MaybeListString(
     type = "TEXT"
     db_api_type = "STRING"
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._scalar_handler = AirtableScalar()
+        self._list_handler = MaybeList(field=self._scalar_handler)
+
     def parse(
         self, value: Optional[AirtableInputTypes]
     ) -> Optional[AirtablePrimitiveTypes]:
         if value is None:
             return None
-
-        if isinstance(value, (str, int, float)):
-            return value
         elif isinstance(value, list):
-            if len(value) == 0:
-                return None
-            elif len(value) == 1:
-                ret = value[0]
-                # TODO(cancan101): Do we have to handle nested arrays / special types?
-                if ret is not None and not isinstance(ret, (str, int, float)):
-                    raise TypeError(f"Unknown type: {type(ret)}")
-                return ret
-            else:
-                raise ValueError("Unable to handle list of length > 1")
-        elif value == {"specialValue": "NaN"}:
+            return self._list_handler.parse(value)
+        else:
+            return self._scalar_handler.parse(value)
+
+
+class MaybeList(
+    Field[List[AirtableRawInputTypes], AirtablePrimitiveTypes]  # type: ignore
+):
+    def __init__(self, field: Field, **kwargs):
+        super().__init__(**kwargs)
+        self.field = field
+        self.type = field.type
+        self.db_api_type = field.db_api_type
+
+    def parse(
+        self, value: Optional[List[AirtableRawInputTypes]]
+    ) -> Optional[AirtablePrimitiveTypes]:
+        if value is None:
             return None
+        elif len(value) == 0:
+            return None
+        elif len(value) == 1:
+            ret = value[0]
+            # TODO(cancan101): Do we have to handle nested arrays?
+            # We handle dict here to allow for "special values"
+            if ret is not None and not isinstance(ret, (str, int, float, dict)):
+                raise TypeError(f"Unknown type: {type(ret)}")
+            return self.field.parse(ret)
+        else:
+            raise ValueError("Unable to handle list of length > 1")
+
+
+class AirtableFloat(
+    Field[AirtableRawNumericInputTypes, Union[float, int]]  # type: ignore
+):
+    """An Airtable float."""
+
+    type = "REAL"
+    db_api_type = "NUMBER"
+
+    def parse(
+        self, value: Optional[AirtableRawNumericInputTypes]
+    ) -> Optional[Union[float, int]]:
+        if isinstance(value, dict):
+            if value == NAN_REPRESENTATION:
+                return math.nan
+            elif value == INF_REPRESENTATION:
+                return math.inf
+            elif value == INF_NEG_REPRESENTATION:
+                return -math.inf
+            else:
+                raise ValueError(f"Unknown float representation: {value}")
+        return value
+
+
+class AirtableScalar(
+    Field[AirtableRawInputTypes, AirtablePrimitiveTypes]  # type: ignore
+):
+    # These types are not really "correct" given the polymorphism
+    type = "TEXT"
+    db_api_type = "STRING"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._string_handler = String()
+        self._float_handler = AirtableFloat()
+
+    def parse(
+        self, value: Optional[AirtableInputTypes]
+    ) -> Optional[AirtablePrimitiveTypes]:
+        if value is None:
+            return None
+        elif isinstance(value, str):
+            return self._string_handler.parse(value)
+        elif isinstance(value, (int, float)):
+            return self._float_handler.parse(value)
+        elif isinstance(value, dict) and len(value) == 1 and SPECIAL_VALUE_KEY in value:
+            return self._float_handler.parse(value)
         else:
             raise TypeError(f"Unknown type: {type(value)}")
--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -1,26 +1,25 @@
-from shillelagh.fields import Boolean, Float, Order, String
+from shillelagh.fields import Boolean, Order, String
 
 from airtabledb import fields
 from airtabledb.adapter import get_airtable_sort, guess_field
 
 
 def test_guess_field():
-    assert type(guess_field([1])) is Float
-    assert type(guess_field([1.5])) is Float
-    assert type(guess_field([1, 1.5])) is Float
+    assert type(guess_field([1])) is fields.AirtableFloat
+    assert type(guess_field([1.5])) is fields.AirtableFloat
+    assert type(guess_field([1, 1.5])) is fields.AirtableFloat
 
     assert type(guess_field([True])) is Boolean
 
     assert type(guess_field(["a"])) is String
 
-    assert type(guess_field([1, {"specialValue": "NaN"}])) is fields.MaybeListString
-    assert type(guess_field([1.5, {"specialValue": "NaN"}])) is fields.MaybeListString
-    assert (
-        type(guess_field([1.5, 1, {"specialValue": "NaN"}])) is fields.MaybeListString
-    )
+    assert type(guess_field([1, {"specialValue": "NaN"}])) is fields.AirtableFloat
+    assert type(guess_field([1.5, {"specialValue": "NaN"}])) is fields.AirtableFloat
+    assert type(guess_field([1.5, 1, {"specialValue": "NaN"}])) is fields.AirtableFloat
 
-    # Not sure if this comes up in practice
-    assert type(guess_field([["a"], ["b"]])) is fields.MaybeListString
+    string_list_field = guess_field([["a"], ["b"]])
+    assert type(string_list_field) is fields.MaybeList
+    assert type(string_list_field.field) is String
 
     # Not sure if this comes up in practice
     assert type(guess_field(["a", 4])) is fields.MaybeListString
